3f1e27bff780f57226c014fd29a4f706aaa6a6aa
leks2023 <109652286+leks2023@users.noreply.github.com>
2024-01-30 13:49:23 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,160 @@
-$(function(){
+$(function() {
 
-	/* Часы */
-	setInterval(function() {
-		$("#timeNow").html(formatDate(new Date()));
-		}, 1000);
-	
-	function formatDate(date) {
-		var hour = "0" + date.getHours(),
-			minute = "0" + date.getMinutes(),
-			second = "0" + date.getSeconds();
-		return [hour.slice(-2), minute.slice(-2), second.slice(-2)].join(":");
-	};
+    /* Nav Toggle on mobile
+    ==========================================*/
 
+    let navToggle = $('#navToggle');
+    let nav = $('#nav');
 
-	$(".column_content3, .column_content5").click(function(){ // задаем функцию при нажатиии на элемент <button>
-		$(".triangle_yellow, .triangle_yellow_right")
-			.fadeOut(750)//  плавно изменяем прозрачность для элемента
-			.fadeIn(750);
-	});
+    navToggle.on('click', function (event) {
+        event.preventDefault();
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+    });
+
+    $(window).on("resize", function() {
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
 
 
-	/* Анимация */
-	$('.rotate').click(function() {
-		$(this).toggleClass('rotated');
-	});
-		var rotation = 0;
-	$('.rotate').click(function() {
-		rotation += 5;
-		$(this).css({'-webkit-transform' : 'rotate('+ rotation +'deg)',
-					'-moz-transform' : 'rotate('+ rotation +'deg)',
-					'-ms-transform' : 'rotate('+ rotation +'deg)',
-					'transform' : 'rotate('+ rotation +'deg)'});
-	});
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let header = $("#header");
+    let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
 
 
-	/* Список событий на странице */
-	$('.column_content').on('click', function() {
-		let $listItems = $('.listItems');
-		let timeAdded = new Date;
-		let num = 0;
-		num = $(".listItems").find("li").length;
-		if((num >=0) && (num <=4)) {
-			$listItems.prepend('<li>' + 'Click на элементе: ' + 'время события: ' + (timeAdded.getHours() + ":" + timeAdded.getMinutes() ) + '</li>');
-		} 
-		else $('.listItems').empty();
-	});
+    /* Header class on scroll 
+    ==========================================*/
+
+    headerScroll();
+
+    $(window).on("scroll resize", function() {
+        headerScroll();
+    });
+
+
+    function headerScroll() {
+        introH = intro.innerHeight();
+        headerH = header.innerHeight();
+        scrollTop = $(this).scrollTop();
+        
+        if (scrollTop >= (introH - headerH)) {
+            header.addClass("header--dark");
+        }
+        else {
+            header.removeClass("header--dark");
+        }
+    }
+
+
+    /* Smooth Scroll to section 
+    ==========================================*/
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let scrollEl = $(this).data("scroll");
+        let scrollElPos = $(scrollEl).offset().top;
+
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+
+        $("html, body").animate({
+            scrollTop: scrollElPos - headerH 
+        }, 500)
+
+    });
+
+
+    /* ScrollSpy 
+    ==========================================*/
+
+    let windowH = $(window).height();
+    ScrollSpy(scrollTop);
+
+    $(window).on("scroll", function() {
+
+    scrollTop = $(this).scrollTop();
+    ScrollSpy(scrollTop);
+
+    });
+
+    function ScrollSpy(scrollTop) {
+
+        $("[data-scrollspy]").each(function() {
+
+            let $this = $(this);
+            let sectionId = $(this).data('scrollspy');
+            let sectionOffset = $this.offset().top;
+            sectionOffset = sectionOffset - (windowH * 0.33333);
+    
+            if(scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll = "' + sectionId + '"]').addClass('active');
+            }
+    
+            if(scrollTop == 0) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+
+        });
+
+    }
+
+
+    /* Slick Slider https://kenwheeler.github.io/slick/ 
+    =================================================*/
+
+    let introSlider = $("#introSlider");
+
+    introSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 2500
+    });
+
+    $("#introSliderPrev").on('click', function () {
+        introSlider.slick('slickPrev');
+    });
+    $("#introSliderNext").on('click', function () {
+        introSlider.slick('slickNext');
+    });
+
+
+
+    /* Aos.js  https://github.com/michalsnik/aos/
+    ===========================================*/
+
+    AOS.init({
+        // Global settings:
+        disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+        
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    });
+
+
+
 
 });
+
